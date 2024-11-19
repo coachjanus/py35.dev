e8ed3f0935fa4af2ed426b34e5d006ec4b0af7f8
coachjanus <coach.qalight@gmail.com>
2024-11-19 21:26:26 +0200

    added lecture 2

--- main.py
+++ main.py
@@ -1,47 +1,82 @@
-# 
+var = "string"
+print(type (var))
+print(id (var))
+True
+False
+None
 
-company_name = "My company"
+# print(2**10000)
 
-def fn():
-    pass
+import math
 
-fn()
+print(math.tan(math.pi))
 
-def getName(name):
-    print(name)
+from decimal import *
 
-getName(company_name)
+getcontext().prec = 20
 
-def sum(a, b):
-    return a + b
+print(Decimal(3.14))
 
-print(sum(55, 55))
+print ("""
+Hello 
+World
 
-a = int(input("Enter a = "))
-b = int(input("Enter b = "))
+""" )
 
-# print(sum(a, b))
+print ('''
 
-o = input("Enter operation: ")
+Hello 
+World
+''')
 
-if o == '+':
-    print(sum(a, b))
-elif o == '-':
-    print(a - b)
-elif o == '*':
-    print(a * b)
-elif o == '/':
-    print(a / b)
-else:
-    print('Ooops! something wrong!')
+print("hello \t world")
 
 
-alert = "division by zero" if a == 0 else "OK"
-print(alert)
+print("hello \t \"world\"")
 
-match o:
-    case '+':
-        print(sum(a, b))
-    case '-':
-        print(a - b)
-    
+print(r'Hi\nHello\tworld')
+print("Hello" + "World")
+
+print("_" * 80)
+
+hello = "Hello World"""
+
+print(hello[7])
+print(hello[0])
+print(hello[len(hello) -1])
+print(min(hello))
+print(max(hello))
+
+print(hello[2:7])
+
+print(hello.split())
+
+print("My first name {} my last name {}".format("Tom", "Cat"))
+
+name = "John"
+last_name = "Doe"
+
+print(f"My name {name} {last_name}")
+
+
+def hell(p):
+    print(f"Hello {p}")
+
+hell("World")
+
+i = 0
+while i < 20:
+    # if i > 10:
+    #     continue
+    print(i)
+    i = i + 1
+
+for c in hello:
+    print(c)
+
+
+while True:
+    o = input("Ender operation: ")
+    if o == 'q':
+        break
+
