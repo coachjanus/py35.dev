159fd2b00b428da3ceeba6194e7632040b9ccae1
coachjanus <coach.qalight@gmail.com>
2025-01-16 11:11:57 +0200

    added staff

--- flask-project/payroll/pages.py
+++ flask-project/payroll/pages.py
@@ -4,12 +4,10 @@ bp = Blueprint('pages', __name__)
 
 @bp.route('/')
 def home():
-    return render_template('pages/index.html')
+    return render_template("pages/home.html")
+    # return "Home page."
 
 @bp.route('/about')
 def about():
-    return render_template('pages/about.html')
-
-@bp.route('/contact')
-def contact():
-    return render_template('pages/contact.html')
+    return render_template("pages/about.html")
+    # return "About page."
